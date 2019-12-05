402bd3f9fb8728916cdbcf79598d43ea64458f9e
adeyinkaakinbola <ashleyakinbola@gmail.com>
2019-12-05 15:04:30 +0300

    implement changes

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = props => {
+  const [state, setState] = useState({
+    count: props.count,
+    text: ""
+  });
+  return (
+    <div>
+      <p>
+        The current{state.text || "count"} is {state.count}
+      </p>
+      <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
+      <button onClick={() => setState({ count: state.count - 1 })}>-1</button>
+      <button onClick={() => setState({ count: 0 })}>reset</button>
+      <input
+        value={state.text}
+        onChange={e => setState({ text: e.target.value })}
+      />
+    </div>
+  );
+};
+App.defaultProps = {
+  count: 0
+};
+ReactDOM.render(<App count={2} />, document.getElementById("data"));
